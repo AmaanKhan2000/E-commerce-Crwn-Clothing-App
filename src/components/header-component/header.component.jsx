196c04d4b139f6de8223a817337455ca4d878010
AmaanKhan2000 <amaankhxn2000@gmail.com>
2021-01-20 19:33:19 +0530

    Imported Logo as a component

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
 import './header.component.styles.scss';
+
+
+const Header = () => (
+  <div className='header'>
+    <Link to= '/' className='logo-container'>
+      
+    </Link>
+  </div>
+);
