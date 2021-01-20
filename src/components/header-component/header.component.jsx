e20140b19284899dab1526681941ba37c56aa209
AmaanKhan2000 <amaankhxn2000@gmail.com>
2021-01-20 19:39:51 +0530

    Made the header component

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+
 import './header.component.styles.scss';
 
 
 const Header = () => (
   <div className='header'>
     <Link to= '/' className='logo-container'>
-      
+      <Logo className='logo' />
     </Link>
+    <div className='options'>
+      <Link to='/shop' className='option'>
+        SHOP
+      </Link>
+      <Link to='/contact' className='option'>
+        CONTACT
+      </Link>
+    </div>
   </div>
 );
+
+export default Header;
