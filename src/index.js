ef8ebcaa7525dd004757cb5036e83b057d8dc4eb
AmaanKhan2000 <amaankhxn2000@gmail.com>
2021-02-09 21:34:17 +0530

    Added Redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from '../src/redux/store'
 import App from './App';
 
 
 ReactDOM.render(
-<BrowserRouter>
-  <App />
-</BrowserRouter>,
+<Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</Provider>,
   document.getElementById('root')
 );
 
