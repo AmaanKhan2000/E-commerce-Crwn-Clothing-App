cd8aae535906203283d0b5f82a9b32200c6081ef
AmaanKhan2000 <amaankhxn2000@gmail.com>
2021-02-17 14:10:27 +0530

    Connect and mapStateToProps

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
        <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
 import {ReactComponent as Logo} from '../../assets/4.2 crown.svg';
 import { auth } from '../firebase/firebase.utils';
 
@@ -31,4 +33,8 @@ const Header = ({currentUser}) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser:state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
